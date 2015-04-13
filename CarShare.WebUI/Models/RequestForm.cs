f6feeb7b702a6d67e2366a0637e6bb3e541bc603
Muhammad Yaseen <mohammad_yaseen13@live.com>
2015-04-13 20:07:07 +0500

    Request form model added, Details now requires auth

---
+++ CarShare.WebUI/Models/RequestForm.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+
+namespace CarShare.WebUI.Models
+{
+    public class RequestForm
+    {
+        [HiddenInput]
+        public int OwnerID { get; set; }
+
+        [HiddenInput]
+        public int RequesterID { get; set; }
+
+        [HiddenInput]
+        public int CarID { get; set; }
+
+    }
+}
